1a9949830b59133ded70485284307cc39c407afe
gorillasz <39219794+gorillasz@users.noreply.github.com>
2018-12-08 09:55:15 +0200

    Update config.js

--- server/config.js
+++ server/config.js
@@ -2,5 +2,5 @@ module.exports = {
     'serverport':5555,
     'tokenexp': 3600,
     'secret': 'mysecretkey',
-    'database': 'mongodb://gorillasz3443:kitpes1488@ds029381.mlab.com:29381/expensedb'
+    'database': 'mongodb://login:password@ds029381.mlab.com:29381/expensedb'
 };
